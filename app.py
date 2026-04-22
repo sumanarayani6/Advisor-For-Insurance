a1177f2d840e8f22813b28aaa446e111c13d91ab
sumanarayani6 <sumlemmecode1@gmail.com>
2026-04-22 20:13:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ from dotenv import load_dotenv
 
 # Semantic Kernel Imports
 from semantic_kernel import Kernel
-#from semantic_kernel.connectors.ai.google.google_ai.services.google_ai_chat_completion import GoogleAIChatCompletion
-from semantic_kernel.connectors.ai.google.gemini import GeminiChatCompletion
+from semantic_kernel.connectors.ai.google.google_ai import GoogleAIChatCompletion
 from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
-from semantic_kernel.connectors.ai.google.google_ai.google_ai_prompt_execution_settings import GoogleAIPromptExecutionSettings
+from semantic_kernel.connectors.ai.google.google_ai import GoogleAIPromptExecutionSettings
 from plugins import InsuranceWorkPlugin, WebSearchPlugin
 
 load_dotenv()
